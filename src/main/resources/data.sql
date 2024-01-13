71df6190b890a16d9a97b92b54639232283dd3cc
Naga Nikhil Bijjala <naganikhil.bijjala@gmail.com>
2024-01-12 19:50:32 -0500

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO student (name, age) VALUES ('Nikhil', 20);
+INSERT INTO student (name, age) VALUES ('Nithin', 20);
+INSERT INTO student (name, age) VALUES ('Nikshith', 20);
+INSERT INTO student (name, age) VALUES ('Chetan', 20);
